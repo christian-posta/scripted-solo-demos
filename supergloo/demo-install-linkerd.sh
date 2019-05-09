d7fdec980b5ddd20db78d024b56c2a31b534913d
Christian Posta <christian.posta@gmail.com>
2019-05-09 15:21:18 +0200

    updated linkerd part of demo

--- supergloo/demo-install-linkerd.sh
+++ supergloo/demo-install-linkerd.sh
@@ -4,12 +4,13 @@
 
 SOURCE_DIR=$PWD
 
-tmux split-window -v -d -c $SOURCE_DIR
-tmux select-pane -t 0
-tmux send-keys -t 1 "stern -n supergloo-system supergloo" C-m
+#tmux split-window -v -d -c $SOURCE_DIR
+#tmux select-pane -t 0
+#tmux send-keys -t 1 "stern -n supergloo-system supergloo" C-m
 
 desc "Installing Linkerd with simple comamnd"
 run "supergloo install linkerd --name linkerd"
 run "kubectl get installs linkerd -n supergloo-system -o yaml"
 run "kubectl get pod -w -n linkerd"
+run "linkerd dashboard"
 backtotop
