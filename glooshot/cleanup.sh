184be4560acf16b292216a8df0429111f9e1d8d9
Christian Posta <christian.posta@gmail.com>
2020-02-19 08:41:56 -0700

    remove clusterrole from cleanupscripts

--- glooshot/cleanup.sh
+++ glooshot/cleanup.sh
@@ -5,3 +5,9 @@ kubectl delete ns istio-system
 killall kubectl
 kubectl delete crd $(kubectl get crd | grep solo)
 kubectl delete crd $(kubectl get crd | grep istio)
+kubectl delete clusterrole $(kubectl get clusterrole | grep glooshot)
+kubectl delete clusterrolebinding $(kubectl get clusterrolebinding | grep glooshot)
+kubectl delete clusterrole $(kubectl get clusterrole | grep istio)
+kubectl delete clusterrolebinding $(kubectl get clusterrolebinding | grep istio)
+kubectl delete clusterrole $(kubectl get clusterrole | grep kiali)
+kubectl delete clusterrolebinding $(kubectl get clusterrolebinding | grep kiali)
--- istio-loop/cleanup.sh
+++ istio-loop/cleanup.sh
@@ -5,4 +5,12 @@ kubectl delete ns calc
 kubectl delete ns squash-debugger
 
 kubectl delete crd $(kubectl get crd | grep solo)
+kubectl delete clusterrole $(kubectl get clusterrole | grep loop)
+kubectl delete clusterrolebinding $(kubectl get clusterrolebinding | grep loop)
+kubectl delete clusterrole $(kubectl get clusterrole | grep istio)
+kubectl delete clusterrolebinding $(kubectl get clusterrolebinding | grep istio)
+kubectl delete clusterrole $(kubectl get clusterrole | grep kiali)
+kubectl delete clusterrolebinding $(kubectl get clusterrolebinding | grep kiali)
+kubectl delete clusterrole $(kubectl get clusterrole | grep squash)
+kubectl delete clusterrolebinding $(kubectl get clusterrolebinding | grep squash)
 killall kubectl
