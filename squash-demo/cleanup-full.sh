42ec74e705f73338da5b5b1eb294d9e42f7ccd4f
Christian Posta <christian.posta@gmail.com>
2020-02-19 09:55:42 -0700

    updated squash reset commands

--- squash-demo/cleanup-full.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-squashctl utils delete-attachments
-kubectl delete deploy example-service1 example-service2-java
-kubectl delete svc example-service1 example-service2-java
-kubectl delete ns squash-debugger
