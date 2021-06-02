3ff69d81c36a5d8b52de99d74f1954268e8cc4b2
Christian Posta <christian.posta@gmail.com>
2021-06-02 10:58:29 -0700

    added initial reset script

---
+++ gloo-mesh/relay/reset.sh
@@ -0,0 +1,12 @@
+source ./env.sh
+
+
+# Get all of the sample apps back online
+kubectl --context $CLUSTER_1 scale deploy/web-api -n istioinaction --replicas=1
+kubectl --context $CLUSTER_2 scale deploy/web-api -n istioinaction --replicas=1
+
+kubectl --context $CLUSTER_1 scale deploy/recommendation -n istioinaction --replicas=1
+kubectl --context $CLUSTER_2 scale deploy/recommendation -n istioinaction --replicas=1
+
+kubectl --context $CLUSTER_1 scale deploy/purchase-history-v1 -n istioinaction --replicas=1
+kubectl --context $CLUSTER_2 scale deploy/purchase-history-v1 -n istioinaction --replicas=1
