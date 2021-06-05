42e75c88a005b42259ba670c2de5eedd65bd9a9e
Christian Posta <christian.posta@gmail.com>
2021-06-05 14:52:03 -0700

    fixed kind command

--- gloo-mesh/relay/scripts/kind/cleanup-kind.sh
+++ gloo-mesh/relay/scripts/kind/cleanup-kind.sh
@@ -1,5 +1,5 @@
-kind delete cluser --name kind1
-kind delete cluser --name kind2
-kind delete cluser --name kind3
+kind delete cluster --name kind1
+kind delete cluster --name kind2
+kind delete cluster --name kind3
 
 rm ~/.kube/config
