eff86e33069de94da4c95f6ae2f507fc35fb0d80
Christian Posta <christian.posta@gmail.com>
2021-06-05 20:53:50 -0700

    added acp to the demo by default

--- gloo-mesh/relay/demo-init.sh
+++ gloo-mesh/relay/demo-init.sh
@@ -1,8 +1,11 @@
 source env.sh
-kubectl --context $MGMT_CONTEXT apply -f resources/virtual-mesh.yaml
+kubectl --context $MGMT_CONTEXT apply -f resources/virtual-mesh-acp.yaml
 
 . ./scripts/check-virtualmesh.sh
 
 ## Create demo namespace for config
 ## All config for the demos will go into this namespace
 kubectl --context $MGMT_CONTEXT create ns demo-config
+
+kubectl --context $MGMT_CONTEXT apply -f resources/failover-config
+kubectl --context $MGMT_CONTEXT apply -f resources/acp-config
