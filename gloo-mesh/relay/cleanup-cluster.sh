bd0e433ece6931a343a2b214b869d91abdb9d8fc
Christian Posta <christian.posta@gmail.com>
2021-06-07 14:02:38 -0700

    fix clean up scripts

--- gloo-mesh/relay/cleanup-cluster.sh
+++ gloo-mesh/relay/cleanup-cluster.sh
@@ -5,18 +5,23 @@ kubectl --context $1 delete ns istioinaction
 
 echo "Uninstall gloo-mesh"
 kubectl --context $1 delete ns gloo-mesh
-kubectl --context $1 delete crd $(kubectl --context $1 get crd | grep mesh.gloo.solo.io) 
+
+echo "Uninstall gloo-fed"
+kubectl --context $1 delete ns gloo-fed
+
+echo "Delete all solo CRDs"
+kubectl --context $1 delete crd $(kubectl --context $1 get crd | grep "solo.io" | awk '{print $1}')
 
 # get rid of any stale clusterrole/binding
 kubectl --context $1 delete clusterrole $(kubectl --context $1 get enterprise-agent | grep wasm-agent | awk '{print $1}') 
 kubectl --context $1 delete clusterrolebinding $(kubectl --context $1 get clusterrolebinding |  grep enterprise-agent | awk '{print $1}')
 
 echo "Uninstall Istio..."
-istioctl1.8 --context $1 x uninstall -y --purge
+istioctl1.9 --context $1 x uninstall -y --purge
 kubectl --context $1 delete ns istio-system
 
 echo "Uninstall Gloo edge"
 helm uninstall gloo-edge -n gloo-system --kube-context $1
 kubectl --context $1 delete ns gloo-system
 
-
+kubectl --context $1 delete ns sleep
--- gloo-mesh/relay/cleanup-mgmt-plane.sh
+++ gloo-mesh/relay/cleanup-mgmt-plane.sh
@@ -1,19 +1,22 @@
 . ./env.sh
 
+echo "Uninstall Gloo Mesh"
 helm uninstall gloo-mesh-enterprise -n gloo-mesh --kube-context $MGMT_CONTEXT
 
-
-kubectl --context $MGMT_CONTEXT delete crd $(kubectl --context $MGMT_CONTEXT get crd | grep "mesh.gloo.solo.io")
-
-kubectl --context $MGMT_CONTEXT delete ns gloo-mesh
-
+echo "Uninstall Federation"
+helm uninstall gloo-fed -n gloo-fed --kube-context $MGMT_CONTEXT
 
 echo "Uninstall Gloo edge"
-helm uninstall gloo-edge -n gloo-system --kube-context $1
-kubectl --context $1 delete ns gloo-system
+helm uninstall gloo-edge -n gloo-system --kube-context $MGMT_CONTEXT
 
-echo "Uninstall Federation"
-helm uninstall gloo-fed -n gloo-system --kube-context $1
+echo "Delete all solo CRDs"
+kubectl --context $MGMT_CONTEXT delete crd $(kubectl --context $MGMT_CONTEXT get crd | grep "solo.io" | awk '{print $1}')
 
+
+# Delete namespaces
+kubectl --context $MGMT_CONTEXT delete ns gloo-fed
 kubectl --context $MGMT_CONTEXT delete ns argocd
 kubectl --context $MGMT_CONTEXT delete ns gogs
+kubectl --context $MGMT_CONTEXT delete ns gloo-system
+kubectl --context $MGMT_CONTEXT delete ns gloo-mesh
+kubectl --context $MGMT_CONTEXT delete ns demo-config
