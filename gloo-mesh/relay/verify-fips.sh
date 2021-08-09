e1f9e4194445145f42cdf307d97d8d1bc52ffa28
Christian Posta <christian.posta@gmail.com>
2021-08-09 11:42:31 -0700

    verify fips

---
+++ gloo-mesh/relay/verify-fips.sh
@@ -0,0 +1,18 @@
+
+source ./env.sh
+. $(dirname ${BASH_SOURCE})/../../util.sh
+
+desc "Press <Enter> to verify data plane"
+read -s
+
+run "kubectl --context $CLUSTER_1 exec -it deploy/east-west-gateway -n istio-system -- pilot-agent request GET server_info --log_as_json|jq {version}"
+
+desc "Press <Enter> to verify control plane"
+read -s
+
+POD=$(kubectl get po -n istio-system | grep istiod | awk '{print $1}')
+rm -f /tmp/pilot-discovery
+run "kubectl cp istio-system/$POD:/usr/local/bin/pilot-discovery /tmp/pilot-discovery && chmod +x /tmp/pilot-discovery"
+
+desc "We should see (boring crypto) as the ssl proivder"
+run "goversion -crypto /tmp/pilot-discovery"
