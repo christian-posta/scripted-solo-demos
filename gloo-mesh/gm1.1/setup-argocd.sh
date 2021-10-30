f96b01387e4616e3aa076acb7926a9fd60c7e63b
Christian Posta <christian.posta@gmail.com>
2021-10-29 17:10:44 -0700

    fixed pw for argo

--- gloo-mesh/gm1.1/setup-argocd.sh
+++ gloo-mesh/gm1.1/setup-argocd.sh
@@ -12,16 +12,14 @@ helm install argo-cd argo/argo-cd --kube-context $MGMT_CONTEXT  -n argocd --vers
 
 kubectl --context $MGMT_CONTEXT rollout status -n argocd deploy/argo-cd-argocd-server 
 
+# Set default pw for argo
+kubectl patch secret -n argocd argocd-secret \
+  -p '{"stringData": { "admin.password": "'$(htpasswd -bnBC 10 "" admin123 | tr -d ':\n')'"}}'
+
 kubectl --context $MGMT_CONTEXT apply -f ./resources/gloo/argocd-vs.yaml
 kubectl --context $MGMT_CONTEXT apply -f ./resources/admin-binding-argo.yaml
 
 echo "Waiting for gloo edge to publish the argo routes...."
 sleep 5s
-
-echo "Username/Password for Argo:"
-echo "Username: admin"
-PW=$(kubectl --context $MGMT_CONTEXT get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
-echo "Admin Username: admin"
-echo "Admin Password: $PW"
 
 kubectl --context $MGMT_CONTEXT apply -f resources/gitops/argocd/gloo-mesh-application.yaml
