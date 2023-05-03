1da77593f25c7c6bab7605b06c81e291df1986d1
christian-posta <christian.posta@gmail.com>
2023-05-03 17:30:00 +0000

    fixed ssh for ui

--- gloo-mesh/gloo-network/ssh-port-forward.sh
+++ gloo-mesh/gloo-network/ssh-port-forward.sh
@@ -5,27 +5,8 @@ BOX=${2:-gmv2}
 ssh -L 8090:localhost:8090 -C -N -l $USER $BOX &
 UI_PID="$!"
 
-ssh -L 8091:localhost:8091 -C -N -l $USER $BOX &
-UI2_PID="$!"
-
-# Gogs
-ssh -L 3000:localhost:3000 -C -N -l $USER $BOX &
-GOGS_PID="$!"
-
-# Argo
-ssh -L 8088:localhost:8088 -C -N -l $USER $BOX &
-ARGO_PID="$!"
-
-# Bookinfo
-ssh -L 8443:172.18.2.1:443 -C -N -l $USER $BOX &
-BOOK_PID="$!"
-
 function cleanup {
   kill -9 $UI_PID
-  kill -9 $UI2_PID  
-  kill -9 $GOGS_PID
-  kill -9 $ARGO_PID
-  kill -9 $BOOK_PID
 }
 
 trap cleanup EXIT
