ec0114b5176856c1f2fe06934299c32dddb6f04e
christian-posta <christian.posta@gmail.com>
2024-06-04 15:57:40 +0000

    fix ssh port fwd

--- api-gw-fed/ssh-port-forward.sh
+++ api-gw-fed/ssh-port-forward.sh
@@ -2,23 +2,9 @@ USER=${1:-solo}
 BOX=${2:-amb}
 
 # UI
-ssh -L 8090:localhost:8090 -C -N -l $USER $BOX &
+ssh -L 3000:localhost:3000 -C -N -l $USER $BOX &
 UI_PID="$!"
 
-ssh -L 8091:localhost:8091 -C -N -l $USER $BOX &
-UI2_PID="$!"
-
-# Gogs
-ssh -L 3000:localhost:3000 -C -N -l $USER $BOX &
-GOGS_PID="$!"
-
-# Argo
-ssh -L 8088:localhost:8088 -C -N -l $USER $BOX &
-ARGO_PID="$!"
-
-# Bookinfo
-ssh -L 8443:172.18.102.1:443 -C -N -l $USER $BOX &
-BOOK_PID="$!"
 
 #Kiali
 ssh -L 20001:localhost:20001 -C -N -l $USER $BOX &
@@ -26,15 +12,11 @@ KIALI_PID="$!"
 
 function cleanup {
   kill -9 $UI_PID
-  kill -9 $UI2_PID  
-  kill -9 $GOGS_PID
-  kill -9 $ARGO_PID
-  kill -9 $BOOK_PID
   kill -9 $KIALI_PID
 }
 
 trap cleanup EXIT
 
 
-echo "Port forwarding to remote box gmv2.... press CTRL-C to end script"
+echo "Port forwarding to remote box amb.... press CTRL-C to end script"
 read -s
