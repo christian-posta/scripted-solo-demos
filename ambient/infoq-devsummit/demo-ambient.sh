9a50b436d6d6a6152e8c01f3a53a169f9bde8650
christian-posta <christian.posta@gmail.com>
2024-06-21 00:19:38 +0000

    automated ambient demo

--- ambient/infoq-devsummit/demo-ambient.sh
+++ ambient/infoq-devsummit/demo-ambient.sh
@@ -6,13 +6,12 @@ SOURCE_DIR=$PWD
 
 
 backtotop
-desc "-----====Demo Istio with Sidecars ====-----"
+desc "-----====Demo Istio with Ambient ====-----"
 read -s
 
 desc "We have three apps: web-api, recommendation, purchase-history"
-run "kubectl get namespaces"
-run "kubectl get po -n web-api"
-run "kubectl exec -it deploy/sleep -- curl -v http://web-api.web-api:8080/"
+desc "(can go check things in k9s)"
+read -s
 
 backtotop
 desc "Let's expose the web-api through the Istio ingress-gateway"
@@ -23,20 +22,13 @@ GATEWAY_IP=$(kubectl get svc -n istio-system | grep ingressgateway | awk '{ prin
 run "curl -H 'Host: istioinaction.io' http://$GATEWAY_IP/"
 
 backtotop
-desc "Let's add workloads to the mesh by injecting the sidecar"
+desc "Let's add workloads to the mesh by labeling the namespaces"
 read -s
 
-run "kubectl label namespace default istio-injection=enabled"
-run "kubectl rollout restart deploy -n default"
-
-run "kubectl label namespace web-api istio-injection=enabled"
-run "kubectl rollout restart deploy -n web-api"
-
-run "kubectl label namespace recommendation istio-injection=enabled"
-run "kubectl rollout restart deploy  -n recommendation"
-
-run "kubectl label namespace purchase-history istio-injection=enabled"
-run "kubectl rollout restart deploy -n purchase-history"
+run "kubectl label namespace default istio.io/dataplane-mode=ambient"
+run "kubectl label namespace web-api istio.io/dataplane-mode=ambient"
+run "kubectl label namespace recommendation istio.io/dataplane-mode=ambient"
+run "kubectl label namespace purchase-history istio.io/dataplane-mode=ambient"
 
 desc "Let's add some load"
 run "./call-web-api-load-gw.sh"
@@ -67,6 +59,12 @@ backtotop
 desc "Controlling the routing to purchase-history"
 read -s
 
+desc "Since we are going to use L7 policies (header, path, etc)"
+desc "We need to deploy a waypoint proxy for purchase history"
+
+run "kubectl apply -f resources/istio/waypoint/purchase-history-ns.yaml"
+run "kubectl label ns purchase-history istio.io/use-waypoint=waypoint"
+
 run "cat ./resources/istio/purchase-history-header-v2.yaml"
 run "kubectl apply -f ./resources/istio/purchase-history-header-v2.yaml"
 
@@ -75,7 +73,7 @@ desc "call the services from another terminal"
 read -s
 
 desc "Now let's call the services with the canary header"
-run "curl -H "x-test-routing: v2"  -H "Host: istioinaction.io" http://$GATEWAY_IP/"
+run "curl -H 'x-test-routing: v2'  -H 'Host: istioinaction.io' http://$GATEWAY_IP/"
 
 
 ####################################
@@ -83,11 +81,8 @@ run "curl -H "x-test-routing: v2"  -H "Host: istioinaction.io" http://$GATEWAY_I
 ####################################
 
 backtotop
-desc "Let's lock down service calls with Policy enforcement and mutual authentication"
+desc "Let's lock down service calls with Policy enforcement"
 read -s
-
-run "cat ./resources/istio/peerauth-strict.yaml"
-run "kubectl apply -f ./resources/istio/peerauth-strict.yaml"
 
 run "cat ./resources/istio/policy/deny-all.yaml "
 run "kubectl apply -f ./resources/istio/policy/deny-all.yaml "
@@ -112,7 +107,8 @@ desc "Let's enable the allow policies for the other services"
 read -s
 
 run "kubectl apply -f resources/istio/policy/allow-web-api-to-recommendation.yaml "
-run "kubectl apply -f resources/istio/policy/allow-recommendation-to-purchistory.yaml"
+
+run "kubectl apply -f resources/istio/policy/waypoint/allow-recommendation-to-purchistory.yaml"
 
 run "curl -H 'Host: istioinaction.io' http://$GATEWAY_IP/"
 
@@ -123,17 +119,19 @@ backtotop
 desc "What about more fine-grained HTTP authz?"
 read -s
 
-run "kubectl exec -it deploy/sleep -- curl -v http://httpbin:8000/headers"
-run "kubectl exec -it deploy/sleep -- curl -v http://httpbin:8000/ip"
+#run "kubectl exec -it deploy/sleep -- curl -v http://httpbin:8000/headers"
+#run "kubectl exec -it deploy/sleep -- curl -v http://httpbin:8000/ip"
 
-desc "But we want to control which paths and how things get called"
-read -s
+# todo: need to implement a waypoint for this...
 
-run "cat ./resources/istio/policy/allow-sleep-to-httpbin.yaml"
-run "kubectl apply -f ./resources/istio/policy/allow-sleep-to-httpbin.yaml"
+#desc "But we want to control which paths and how things get called"
+#read -s
 
-run "kubectl exec -it deploy/sleep -- curl -v http://httpbin:8000/ip"
-run "kubectl exec -it deploy/sleep -- curl -v -H 'x-test-me: approved' http://httpbin:8000/headers"
+#run "cat ./resources/istio/policy/allow-sleep-to-httpbin.yaml"
+#run "kubectl apply -f ./resources/istio/policy/allow-sleep-to-httpbin.yaml"
+
+#run "kubectl exec -it deploy/sleep -- curl -v http://httpbin:8000/ip"
+#run "kubectl exec -it deploy/sleep -- curl -v -H 'x-test-me: approved' http://httpbin:8000/headers"
 
 ####################################
 # jwt policy
@@ -142,11 +140,11 @@ backtotop
 desc "Let's add a JWT policy to be able to call purchase history"
 read -s
 
-run "cat ./resources/istio/policy/request-authentication.yaml"
-run "cat .resources/istio/policy/allow-recommendation-to-purchase-history-jwt.yaml"
+run "cat ./resources/istio/policy/waypoint/request-authentication-purchase-history.yaml"
+run "cat ./resources/istio/policy/waypoint/allow-recommendation-to-purchistory-jwt.yaml"
 
-run "kubectl apply -f ./resources/istio/policy/request-authentication.yaml"
-run "kubectl apply -f .resources/istio/policy/allow-recommendation-to-purchase-history-jwt.yaml"
+run "kubectl apply -f ./resources/istio/policy/waypoint/request-authentication-purchase-history.yaml"
+run "kubectl apply -f ./resources/istio/policy/waypoint/allow-recommendation-to-purchistory-jwt.yaml"
 
 run "curl -H 'Host: istioinaction.io' http://$GATEWAY_IP/"
 
