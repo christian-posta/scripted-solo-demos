ad891f0cde7ce3e024b07c1067463390f9bf897c
christian-posta <christian.posta@gmail.com>
2025-01-08 23:40:24 +0000

    updated script to ssh to grafana

--- ai-gateway/ssh-port-forward.sh
+++ ai-gateway/ssh-port-forward.sh
@@ -2,11 +2,15 @@ USER=${1:-solo}
 BOX=${2:-gmv2}
 
 
+GRAFANA_IP=$(ssh $USER@$BOX "kubectl get svc kube-prometheus-stack-grafana  -n monitoring -o jsonpath='{.status.loadBalancer.ingress[0].ip}'")
+
+GW_IP=$(ssh $USER@$BOX "kubectl get svc gloo-proxy-ai-gateway  -n gloo-system -o jsonpath='{.status.loadBalancer.ingress[0].ip}'")
+
 # Gateway
-ssh -L 8080:172.18.101.1:8080 -C -N -l $USER $BOX &
+ssh -L 8080:$GW_IP:8080 -C -N -l $USER $BOX &
 GW_PID="$!"
 
-ssh -L 3000:172.18.101.2:3000 -C -N -l $USER $BOX &
+ssh -L 3000:$GRAFANA_IP:3000 -C -N -l $USER $BOX &
 GRAF_PID="$!"
 
 function cleanup {
