8045ecd6e69c6b631ee2b4e15ddd1f93f5efbe51
christian-posta <christian.posta@gmail.com>
2025-01-15 00:17:19 +0000

    checkpoint loki integration

--- ai-gateway/setup-observability.sh
+++ ai-gateway/setup-observability.sh
@@ -14,7 +14,7 @@ helm upgrade --install opentelemetry-collector open-telemetry/opentelemetry-coll
 -f ./resources/observability/helm-otel-install.yaml
 
 
-# Set up Prometheus and Grafana
+# Set up Prometheus and Grafana for the kube-prometheus-stack
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
@@ -25,10 +25,10 @@ prometheus-community/kube-prometheus-stack \
 --create-namespace \
 --values ./resources/observability/helm-prom-install.yaml
 
-
+# set up the pod monitor for the otel collector
 kubectl apply -f ./resources/observability/pod-monitor.yaml -n otel
 
-
+# install observability dashboards
 kubectl -n monitoring create cm envoy-dashboard --from-file=./resources/observability/envoy.json
 kubectl label -n monitoring cm envoy-dashboard grafana_dashboard=1
 
@@ -38,10 +38,19 @@ kubectl label -n monitoring cm gg-dashboard grafana_dashboard=1
 kubectl -n monitoring create cm llm-dashboard --from-file=./resources/observability/llm-dashboard.json
 kubectl label -n monitoring cm llm-dashboard grafana_dashboard=1
 
+
+# Install Jaeger for tracing
 helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
 helm upgrade -i -n monitoring jaeger jaegertracing/jaeger --values ./resources/observability/jaeger.yaml
 kubectl apply -f ./resources/observability/jaeger-upstream.yaml -n gloo-system
 
 kubectl expose deployment jaeger --type=LoadBalancer --name=jaeger-loadbalancer --port=16686 --target-port=16686 -n monitoring
 
+# Install Loki for logging
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+helm upgrade --install loki grafana/loki --namespace monitoring --values ./resources/observability/loki-install.yaml
+
+# set up the ai-gateway parameters with tracing tags and metrics
 kubectl apply -f ./resources/observability/ai-gateway-parameters.yaml -n gloo-system
