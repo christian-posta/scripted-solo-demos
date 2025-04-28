022e98f10cdb641a81ec2c233ae5048eb0b2cb19
Christian Posta <christian.posta@gmail.com>
2025-04-27 18:35:29 -0700

    port forward the metrics port

--- agentgateway/run-proxy.sh
+++ agentgateway/run-proxy.sh
@@ -2,4 +2,4 @@ DEFAULT_CONFIG="resources/basic.json"
 CONFIG_LOCATION=${1:-$DEFAULT_CONFIG}
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 echo "The script is located in: $SCRIPT_DIR"
-docker run -it -e "RUST_BACKTRACE=full" -e "RUST_BACKTRACE=1" -p 3000:3000 -p 19000:19000 -v $SCRIPT_DIR/resources:/app/resources agentproxy-demo -f /app/${CONFIG_LOCATION}
+docker run -it -e "RUST_BACKTRACE=full" -e "RUST_BACKTRACE=1" -p 9091:9091-p 3000:3000 -p 19000:19000 -v $SCRIPT_DIR/resources:/app/resources agentproxy-demo -f /app/${CONFIG_LOCATION}
