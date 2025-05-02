bef055c58445d9b07f05252855697e1789b475a8
Christian Posta <christian.posta@gmail.com>
2025-05-02 00:24:14 +0000

    added ollama into the demo; still has some issues with converting the tool call, but fixing that shortly

--- kagent/setup-kagent.sh
+++ kagent/setup-kagent.sh
@@ -5,6 +5,11 @@ source ~/bin/ai-keys
 
 # helm search repo oci://ghcr.io/kagent-dev/kagent/helm/kagent --versions
 
+helm install kagent-crds oci://ghcr.io/kagent-dev/kagent/helm/kagent-crds \
+    --namespace kagent \
+    --create-namespace \
+    --version $KAGENT_VERSION
+
 helm upgrade --install kagent oci://ghcr.io/kagent-dev/kagent/helm/kagent \
     --namespace kagent \
     --create-namespace \
