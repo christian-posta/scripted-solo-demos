19c628539f68e41d690fb51b7041c476afe87b66
Christian Posta <christian.posta@gmail.com>
2025-07-21 10:01:25 -0700

    updated agent gateway + kagent

--- kagent/agentgateway/reset-configmaps.sh
+++ kagent/agentgateway/reset-configmaps.sh
@@ -2,7 +2,7 @@ kubectl delete cm agentgateway-config -n kagent
 kubectl delete cm openapi-config -n kagent
 kubectl delete cm pubkey-config -n kagent
 
-kubectl create -n kagent configmap agentgateway-config --from-file=config.json=config.json
+kubectl create -n kagent configmap agentgateway-config --from-file=config.yaml=config.yaml
 kubectl create -n kagent configmap openapi-config --from-file=petstore.json=petstore.json
 kubectl create -n kagent configmap pubkey-config --from-file=pubkey=pubkey
 
