64c65420331c07e8eb526bbf99aade5c9657adc0
Christian Posta <christian.posta@gmail.com>
2025-10-23 15:40:46 -0700

    updated to use claude

--- agentgateway/run-proxy.sh
+++ agentgateway/run-proxy.sh
@@ -1,10 +1,3 @@
-
-DEFAULT_CONFIG="resources/basic.yaml"
-
-IMAGE="ghcr.io/agentgateway/agentgateway:0.6.2-ext"
-
-CONFIG_LOCATION=${1:-$DEFAULT_CONFIG}
-SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-
-echo "The script is located in: $SCRIPT_DIR"
-docker run -it -e "RUST_BACKTRACE=full" -e "RUST_BACKTRACE=1" -p 5555:5555 -p 15020:15020 -p 3000:3000 -p 15000:15000 -v $SCRIPT_DIR/resources:/app/resources $IMAGE -f /app/${CONFIG_LOCATION}
+source ~/bin/ai-keys
+export OPEN_API_SCHEMA_PATH=/Users/christian.posta/scripted-demos/agentgateway/resources/openapi/petstore.json
+agentgateway -f config/agentgateway_config.yaml
