be2d59a6d1bf0d01fe215e6428b9df35e36b11b6
Christian Posta <christian.posta@gmail.com>
2026-01-07 14:41:49 -0700

    updated bedrock examples for both standalone and k8s

--- agentgateway/enterprise/setup-llms.sh
+++ agentgateway/enterprise/setup-llms.sh
@@ -11,6 +11,26 @@ kubectl create secret generic anthropic-secret -n enterprise-agentgateway \
 --from-literal="Authorization=$ANTHROPIC_API_KEY" \
 --dry-run=client -oyaml | kubectl apply -f -
 
+kubectl apply -f ./resources/llm/anthropic.yaml
+
 kubectl create secret generic gemini-secret -n enterprise-agentgateway \
 --from-literal="Authorization=$GEMINI_API_KEY" \
 --dry-run=client -oyaml | kubectl apply -f -
+
+kubectl apply -f ./resources/llm/gemini.yaml
+
+
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: bedrock-secret
+  namespace: enterprise-agentgateway
+type: Opaque
+stringData:
+  accessKey: ${AWS_ACCESS_KEY_ID}
+  secretKey: ${AWS_SECRET_ACCESS_KEY}
+  sessionToken: ${AWS_SESSION_TOKEN}
+EOF
+
+kubectl apply -f ./resources/llm/bedrock.yaml
---
+++ agentgateway/enterprise/setup-mcp.sh
@@ -0,0 +1,5 @@
+source .env
+
+
+kubectl apply -f ./resources/mcp/public.yaml
+
