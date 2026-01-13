5c46693e58c0c41c3d708ec4015db03f53b1bdbd
Christian Posta <christian.posta@gmail.com>
2026-01-13 14:58:11 -0700

    got entra mcp authentication working for enterprise

--- agentgateway/enterprise/setup-mcp.sh
+++ agentgateway/enterprise/setup-mcp.sh
@@ -1,4 +1,6 @@
+set -a
 source .env
+set +a
 
 
 kubectl apply -f ./resources/mcp/public.yaml
@@ -6,3 +8,7 @@ kubectl apply -f ./resources/mcp/jwt-secure.yaml
 kubectl apply -f ./resources/mcp/public-oauth.yaml
 kubectl apply -f ./resources/mcp/mcp-oidc.yaml
 
+# Use envsubst to substitute environment variables (like $AUTH0_CLIENT_SECRET) in the YAML
+envsubst < ./resources/mcp/mcp-oidc-extauth.yaml | kubectl apply -f -
+
+envsubst < ./resources/mcp/public-oauth-entra.yaml | kubectl apply -f -
