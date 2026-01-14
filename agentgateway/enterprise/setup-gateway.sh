15348a2bf3a61caecb0805ec2c1d076deb71546b
Christian Posta <christian.posta@gmail.com>
2026-01-14 13:33:49 -0700

    updated instructions to install agw UI

--- agentgateway/enterprise/setup-gateway.sh
+++ agentgateway/enterprise/setup-gateway.sh
@@ -38,3 +38,54 @@ kubectl apply -f ./resources/setup/gateway.yaml
 # Optional: dummy failover service (used by /failover/openai route demo)
 kubectl apply -f ./resources/supporting/failover-429.yaml
 
+
+
+# Install AgentGateway UI which helps surface the elicitaitons
+export KEYCLOAK_IP=34.23.181.61
+export OIDC_BACKEND=kagent-backend
+export OIDC_FRONTEND=kagent-ui
+export BACKEND_CLIENT_SECRET=$KEYCLOAK_BACKEND_CLIENT_SECRET
+export ENDPOINT=https://demo-keycloak-907026730415.us-east4.run.app/realms/kagent-dev/protocol/openid-connect
+export authEndpoint=https://demo-keycloak-907026730415.us-east4.run.app/realms/kagent-dev/protocol/openid-connect/auth
+export tokenEndpoint=https://demo-keycloak-907026730415.us-east4.run.app/realms/kagent-dev/protocol/openid-connect/token
+export logoutEndpoint=https://demo-keycloak-907026730415.us-east4.run.app/realms/kagent-dev/protocol/openid-connect/logout
+export OIDC_ISSUER=https://demo-keycloak-907026730415.us-east4.run.app/realms/kagent-dev
+# export KAGENT_ENT_VERSION=0.1.10-2026-01-06-main-ba15b13f
+
+# export KAGENT_MGMT_CHART=oci://us-docker.pkg.dev/developers-369321/solo-enterprise-public-nonprod/charts/management
+export KAGENT_ENT_VERSION=0.2.1
+export KAGENT_MGMT_CHART=oci://us-docker.pkg.dev/solo-public/solo-enterprise-helm/charts/management
+
+helm upgrade -i kagent-mgmt \
+ $KAGENT_MGMT_CHART \
+-n kagent --create-namespace \
+--version "$KAGENT_ENT_VERSION" \
+-f - <<EOF
+imagePullSecrets: []
+global:
+  imagePullPolicy: IfNotPresent
+oidc:
+  issuer: ${OIDC_ISSUER}
+rbac:
+  roleMapping:
+    roleMapper: "claims.Groups.transformList(i, v, v in rolesMap, rolesMap[v])"
+    roleMappings:
+      admins: "global.Admin"
+      readers: "global.Reader"
+      writers: "global.Writer"
+service:
+  type: LoadBalancer
+  clusterIP: ""
+ui:
+  backend:
+    oidc:
+      clientId: ${OIDC_BACKEND}
+      secret: ${BACKEND_CLIENT_SECRET}
+  frontend:
+    oidc:
+      clientId: ${OIDC_FRONTEND}
+clickhouse:
+  enabled: true
+tracing:
+  verbose: true
+EOF
